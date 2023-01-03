caf34b891ff5c25dcee100f851a2cabc1240ee09
josefineschaefer <josefine.schaefer@storyblok.com>
2023-01-03 10:24:06 +0100

    initial commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<header class="w-full h-24 bg-[#f7f6fd]">
+	<div class="container h-full mx-auto flex items-center justify-between">
+		<a href="home">
+			<h1 class="text-[#50b0ae] text-3xl font-bold">Storyblok SvelteKit</h1>
+		</a>
+		<nav>
+			<ul class="flex space-x-8 text-lg font-bold">
+				<li class="hover:text-[#50b0ae]">
+					<a href="#"> Menu Item </a>
+				</li>
+				<li class="hover:text-[#50b0ae]">
+					<a href="#"> Menu Item </a>
+				</li>
+				<li class="hover:text-[#50b0ae]">
+					<a href="#"> Menu Item </a>
+				</li>
+				<li class="hover:text-[#50b0ae]">
+					<a href="#"> Menu Item </a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+</header>
